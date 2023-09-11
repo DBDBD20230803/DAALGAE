c076c50c13697bfd82680d88f58dfc2dec8e9405
somang1206 <dlthakd1206@naver.com>
2023-09-11 22:22:54 +0900

    모바일 loginFindId 991까지

--- myPage/js/mypage.js
+++ myPage/js/mypage.js
@@ -1,20 +1,26 @@
 const addPetBtn = document.querySelector(".addPet");
 const dogInfo = document.querySelector(".dogInfo");
 
-addPetBtn.addEventListener('click', function() {
+/*addPetBtn.addEventListener('click', function() {
     dogInfo.style.display = 'block';
-});
+});*/
 
-const spreadBtn1 = document.querySelector(".spreadBtn1");
-const spreadBtn2 = document.querySelector(".spreadBtn2");
 
-spreadBtn1.addEventListener('click', function() {
-    if( spreadBtn2.style.display == 'block' ){
-        spreadBtn1.style.display = 'none';
+
+const spreadBtn = document.querySelector(".spreadBtn");
+
+spreadBtn.addEventListener('click', function (){
+
+    if( dogInfo.style.display === 'none'){
         dogInfo.style.display = 'block';
-        spreadBtn2.style.display ='block';
+        spreadBtn.innerHTML = "접기 <i class='xi-angle-up-thin xi-x'></i>";
+
     }else{
-        spreadBtn2.style.display = 'block';
+        dogInfo.style.display = 'none';
+        spreadBtn.innerHTML = "펼치기 <i class='xi-angle-down-thin xi-x'></i>";
+
+
     }
-   
+
 });
+

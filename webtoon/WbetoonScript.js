4f0f21d8a479628b1f3423ef0a310a8b2280876b
lSuns <lsuns@lSunsui-MacBook-Pro.local>
2023-09-11 09:07:46 +0900

    20230911-sh004

---
+++ webtoon/WbetoonScript.js
@@ -0,0 +1,7 @@
+const wtBoxButton1 = document.getElementById('wtBoxButton1');
+wtBoxButton1.addEventListener("click", function (){
+wtBoxButton1.style.cursor = 'pointer';
+    console.log('click 이벤트 발생');
+    alert('개껌 2개가 차감됩니다.')
+});
+
